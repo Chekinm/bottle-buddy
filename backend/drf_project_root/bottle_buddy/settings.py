92471d464024f6380c774d0788e951cf96e17de3
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-06-30 02:58:50 +0300

    s

--- backend/drf_project_root/bottle_buddy/settings.py
+++ backend/drf_project_root/bottle_buddy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", config('DJANGO_SECRET_KEY'))
+SECRET_KEY = os.environ.get("SECRET_KEY", "234234233454jhksjdhfFDFsd234234234")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
